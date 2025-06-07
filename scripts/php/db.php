a13f9c64ed0410fd7d2738cb938dc547cd0c97a3
Tomáš Richtr <144466276+Tomajto@users.noreply.github.com>
2025-06-07 18:25:36 +0200

    Add database connection script: initialize MySQL connection with environment variables

---
+++ scripts/php/db.php
@@ -0,0 +1,14 @@
+<?php
+$host = 'localhost';
+$dbname = getenv('DB_NAME'); 
+$username = getenv('DB_USER');   
+$password = getenv('DB_PASS'); 
+
+$conn = new mysqli($host, $username, $password, $dbname);
+
+
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
