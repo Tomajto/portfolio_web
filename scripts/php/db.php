4083f0a21154d3a530de6c2f79482de1a22f392b
Tomáš Richtr <144466276+Tomajto@users.noreply.github.com>
2025-06-07 18:54:56 +0200

    Update database connection script: use environment variables for credentials

--- scripts/php/db.php
+++ scripts/php/db.php
@@ -1,12 +1,13 @@
 <?php
-$host = 'localhost';              // leave this as localhost
-$dbname = 'portfoliodb';          // your actual database name
-$username = 'tomajto';     // the username you created in MySQL
-$password = 'petrpavel397'; // the password you set
+$host = 'localhost';
+$dbname = getenv('DB_NAME'); 
+$username = getenv('DB_USER');   
+$password = getenv('DB_PASS'); 
 
 $conn = new mysqli($host, $username, $password, $dbname);
 
-// Check connection
+
+
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
