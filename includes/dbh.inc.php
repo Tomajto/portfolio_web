8847ea9d6e664d3a7a25df67684752d7a635f5ee
Tomáš Richtr <144466276+Tomajto@users.noreply.github.com>
2025-06-08 16:57:17 +0200

    Fix PDO connection string syntax for improved readability

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -6,7 +6,7 @@ $password = getenv('DB_PASS');
 $pdo = "";
 
 try {
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    $pdo = new PDO("mysql:host={$host};dbname={$dbname}", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
     echo "Connection Successful to the database: {$dbname} with host {$host}, logged in as {$username}";
