031baa55664c129d958d9c19b3001ab65ee771f1
Tomáš Richtr <144466276+Tomajto@users.noreply.github.com>
2025-06-12 22:17:33 +0200

    Remove error reporting settings from leaderboard page

--- screens/leaderboard.php
+++ screens/leaderboard.php
@@ -1,8 +1,7 @@
 <?php
 // filepath: c:\PROJEKTY\portfolio_web\screens\leaderboard.php
 session_start();
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
+
 
 include_once __DIR__ . '/../database/db_connection.php';
 
